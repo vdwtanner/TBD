53b6b94b9e1ae1d1c73a2f329269085c0f729889
vdwtanner <vdwtanner@gmail.com>
2017-08-07 22:04:18 -0500

    Added TerrainGenerator.cs  and tossed some basic shit into Generation manager

--- ProcAdvUnity/Assets/Scripts/Generation/GenerationManager.cs
+++ ProcAdvUnity/Assets/Scripts/Generation/GenerationManager.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GenerationManager : MonoBehaviour {
+    public int seed = 0;
+
+    protected BiomeGenerator m_biomeGenerator;
+    protected CityStateGenerator m_cityStateGenerator;
+    protected TerrainGenerator m_terrainGenerator;
+
+    private static GenerationManager s_instance;
+
+    void Awake() {
+        s_instance = this;
+    }
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    static GenerationManager GetInstance() {
+        return s_instance;
+    }
 }
