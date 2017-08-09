ec99825faded806062017ba022e70d990b7b04dd
vdwtanner <vdwtanner@gmail.com>
2017-08-08 23:30:07 -0500

    Moved perlin noise, basically just committing to keep structure the same

---
+++ ProcAdvUnity/Assets/Scripts/PreGeneration/ResourceGeneration/ResourceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGenerator {
+    private int m_numNodes;
+
+    ResourceGenerator(int numNodes)
+    {
+        m_numNodes = numNodes;
+    }
+}
